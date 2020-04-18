1dddd170f8d3a39a97b4fbdabf5ffe08440e491c
David Giraldo <davidgiraldobueno@gmail.com>
2020-04-18 14:38:09 -0500

    readme

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Cards from './components/Cards/cards';
 
 class App extends React.Component {
     render() {
         return(
             <div>
                 <h1>Hello World</h1>
+                <Cards />
             </div>
         );
     }
---
+++ src/components/Cards/Cards.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Cards extends React.Component {
+    render(){
+        return (
+            <div>
+                Hello from Cards
+            </div>
+        );
+    }
+}
+
+export default Cards;
